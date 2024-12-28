51d10bbac41bd2a3826d389a3181399f506c4a11
ParvatiIrlapalle17 <apple@Apples-MacBook-Air.local>
2024-12-28 16:57:21 +0530

    Only Mac target Project Crreation

---
+++ mac/MacLearn/MacLearn/MainView/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  MacLearn
+//
+//  Created by Apple on 28/12/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationView {
+            HomeView()
+             SideView()
+        }
+      
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ mac/MacLearn/MacLearn/Side Menu/SideView.swift
@@ -0,0 +1,18 @@
+//
+//  SeideView.swift
+//  MacLearn
+//
+//  Created by Apple on 28/12/24.
+//
+
+import SwiftUI
+
+struct SideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideView()
+}
